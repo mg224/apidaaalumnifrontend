14e19e93790492a6d675fd8cf0031752c31d117c
Misha Gupta <msg0022@outlook.com>
2025-04-15 21:27:21 -0400

    removed unneccesary pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 import React, { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import './index.css'
-import PastRoles from './pages/PastRoles'
-import Industry from './pages/Industry'
-import GradYear from './pages/GradYear'
 import Home from './pages/Home'
-import Name from './pages/Name'
-import Results from './pages/Results'
 import { Navbar } from './components/Navbar'
 import { MobileMenu } from './components/MobileMenu'
 import Search from './pages/Search'
@@ -22,11 +17,6 @@ function App() {
       <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/name" element={<Name />} />
-        <Route path="/gradyear" element={<GradYear />} />
-        <Route path="/pastRoles" element={<PastRoles />} />
-        <Route path="/industry" element={<Industry />} />
-        <Route path="/data" element={<Results />} /> */}
         <Route path="/search" element={<Search />} />
       </Routes>
     </>
